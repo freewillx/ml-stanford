21e8c0905f2931a04c17588f4b4a40810ffedc21
Will Zhang <will.zhang80@gmail.com>
2015-07-24 23:33:25 -0600

    Ex1 multi Gradient Descent

--- ex1/computeCostMulti.m
+++ ex1/computeCostMulti.m
@@ -13,7 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+predections = X * theta;
 
+J = sum((predections - y) .^ 2)/ (2 * m);
 
 
 
