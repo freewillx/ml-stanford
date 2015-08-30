cfd667b3f6c8cb1173b3b1b377b734475083c3bf
Will Zhang <will.zhang80@gmail.com>
2015-08-30 00:48:55 -0600

    ex6 Example Dataset3 & ex6 bug fixes

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -9,7 +9,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 
 % You need to return the following variables correctly.
 C = 1;
-sigma = 0.3;
+sigma = 0.1;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,10 +23,34 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% Brute-force searching for best C and sigma
+% Commented out code for exe efficiency once C and sigma is found
 
-
-
-
+%cList = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+%sigmaList = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+%
+%predError = -1;
+%
+%for c_best = cList
+% for sigma_best = sigmaList
+%
+%  model= svmTrain(X, y, c_best, @(x1, x2) gaussianKernel(x1, x2, sigma_best));
+%  predictions = svmPredict(model, Xval);
+%  meanPredError = mean(double(predictions ~= yval));
+%
+%  if (predError == -1 || predError > meanPredError)
+%    predError = meanPredError;
+%    C = c_best;
+%    sigma = sigma_best;
+%  endif
+%
+% endfor
+%endfor
+%
+%disp('found min predError')
+%disp(predError)
+%disp(C)
+%disp(sigma)
 
 
 % =========================================================================
